e7698b7ab08927a30cea52b300c72bcb4c82bf4e
KarlHarnois <karl@goDynamo.com>
2019-07-17 14:55:44 -0400

    Add missing lastname test to player model

--- tests/unit/test_player.py
+++ tests/unit/test_player.py
@@ -6,4 +6,6 @@ from mox.models import Player
 def test_create():
     attributes = { 'firstname': 'Binom', 'lastname': 'Missieu' }
     player = Player.create(attributes)
+
     assert player.firstname == 'Binom'
+    assert player.lastname == 'Missieu'
