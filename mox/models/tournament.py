9132712ca5f53ae3c832c97ff28d6d9e4e965187
jupouliot <jupouliot@users.noreply.github.com>
2019-07-18 12:09:58 -0400

    Tournament creation groundwork (#23)
    * Tournament creation groundwork
    
    Added tournament model object and routes
    
    * Corrected linebreaks

---
+++ mox/models/tournament.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Tournament:
+    def __init__(self, name, competitors):
+        self.name = name
+        self.competitors = competitors
+
+    @staticmethod
+    def create(attributes):
+        return Tournament(
+            name=attributes['name'],
+            competitors=attributes['competitors']
+        )
+
+    def serialize(self):
+        return {
+            'name': self.name
+        }
