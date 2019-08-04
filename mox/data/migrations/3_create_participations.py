5144350239fd335685eecd69ea885b6e520c7ecb
BreakfastAtKyrils <kyrildr@gmail.com>
2019-08-04 15:45:55 -0400

    create participations table
    we want to associate players to a tournament, and the problem is that both belong to each other multiple times, hence we need a table to keep track.

---
+++ mox/data/migrations/3_create_participations.py
@@ -0,0 +1,17 @@
+def up(database):
+    database.execute("""
+    CREATE SEQUENCE participations_id_seq;
+
+    CREATE TABLE participations (
+        id integer NOT NULL DEFAULT nextval('participations_id_seq'),
+        player_id integer NOT NULL, 
+        tournament_id integer NOT NULL
+    );
+
+    ALTER SEQUENCE participations_id_seq
+    OWNED BY participations.id;
+    """)
+
+
+def down(database):
+    database.execute('DROP TABLE participations;')
