46e2c3e12557798a30698c467908516598cfa0c8
KarlHarnois <karl@goDynamo.com>
2019-08-27 18:37:59 -0400

    Implement equality for Player model

--- mox/models/player.py
+++ mox/models/player.py
@@ -10,3 +10,11 @@ class Player:
             'firstname': self.firstname,
             'lastname': self.lastname
         }
+
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.id == other.id \
+                and self.firstname == other.firstname \
+                and self.lastname == other.lastname
+        else:
+            return False
